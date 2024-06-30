d0499bf35e01546f66aad1ecc9017c0afd7f7632
Thet Mhue Wai <waimhuethet@gmail.com>
2024-06-30 17:38:12 +0630

    message changes

--- Kotlin Learning FirstProgram/car.kt
+++ Kotlin Learning FirstProgram/car.kt
@@ -1,10 +1,23 @@
-class Car(var brand:String, var year:Int)
+class Car(var brand:String, var year:Int){
+    fun drive(){
+        println("Driving... $brand")
+    }
+    fun speed(s:Int){
+        println("Speed of $brand is $s.")
+    }
+}
 
 fun main() {
     var c1 = Car("Volvo",1990)
     var c2 = Car("Toyota",2000)
 
     c1.year = 1999
+
     println("First Car is: ${c1.brand} and ${c1.year}")
     println("Second Car is: ${c2.brand} and ${c2.year}")
+
+    c1.drive()
+    c2.drive()
+    c1.speed(100)
+    c2.speed(80)
 }
